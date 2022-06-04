a1c0cc92885f23f84ba962f2dcb27236cd576cc5
jungHyeonS <fbtls25@naver.com>
2022-06-04 17:34:24 +0900

    셀프넘버

--- function.py
+++ function.py
@@ -1,7 +1,19 @@
-def solve(a:list):
-    sum = 0
-    for i in a:
-        sum = sum + i
-    return sum
+# def solve(a:list):
+#     sum = 0
+#     for i in a:
+#         sum = sum + i
+#     return sum
 
-solve([1,2,3,4,5])
+# solve([1,2,3,4,5])
+
+
+# numbers = set(range(1, 10000))
+# remove_set = set()
+# for num in numbers :
+#     for n in str(num):
+#         num += int(n)
+#     remove_set.add(num)
+
+# self_numbers = numbers - remove_set 
+# for self_num in sorted(self_numbers):
+#     print(self_num)
