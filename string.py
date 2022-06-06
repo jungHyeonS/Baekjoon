143fec15896cc525ca126d2b71af79d263f244e3
jungHyeonS <fbtls25@naver.com>
2022-06-06 13:06:34 +0900

    문자열 - 숫자의합,알파벳찾기,문자열반복

--- string.py
+++ string.py
@@ -1,1 +1,36 @@
-print(ord(input()))
+# print(ord(input()))
+
+# import sys
+# num1 = int(sys.stdin.readline())
+# chd = sys.stdin.readline().strip()
+# sum = 0
+# for i in list(chd):
+#     sum = sum + int(i)
+
+# print(sum)
+
+
+# import sys
+# chd = sys.stdin.readline().strip()
+# aplah = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# for i in range(0,len(aplah)):
+#     for j in range(0,len(chd)):
+#         a = -1
+#         if aplah[i] == chd[j]:
+#             a = j
+#             break
+#     print(a,end=" ")
+
+
+import sys
+num1 = int(sys.stdin.readline())
+for i in range(0,num1):
+    chd = sys.stdin.readline().strip().split(" ")
+    for j in range(0,len(chd[1])):
+        print(chd[1][j] * int(chd[0]),end="")
+    print("")
+        
+
+
+
+
