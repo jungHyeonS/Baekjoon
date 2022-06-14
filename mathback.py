72f7b448a9a9e73ab9c02f3f4198ccca035d533a
jungHyeonS <fbtls25@naver.com>
2022-06-14 22:57:29 +0900

    백준 벌집 및 분수 찾기 진행중

--- mathback.py
+++ mathback.py
@@ -1,7 +1,38 @@
-import sys
-num1,num2,num3 = map(int,sys.stdin.readline().split())
+# import sys
+# num1,num2,num3 = map(int,sys.stdin.readline().split())
 
-if num2 >= num3:
-    print("-1")
-else:
-    print(int(num1 / (num3 - num2)+1))
+# if num2 >= num3:
+#     print("-1")
+# else:
+#     print(int(num1 / (num3 - num2)+1))
+
+
+# 벌집
+# num1 = int(input())
+
+# polygon = 1
+# cnt = 1
+# while num1 > polygon:
+#     polygon += 6 * cnt
+#     cnt = cnt + 1
+
+# print(cnt)
+# 1
+# 2,3,4,5,6,7
+# 8,9,10,11,12,13,14,15,16,17,18,19
+
+
+
+# num1 = int(input())
+
+# 1 = 1/1
+# 2 = 1/2
+
+
+numerator = 0
+denominator = 0
+for i in range(1,10):
+    for j in range(1,i):
+        print(j)
+
+
