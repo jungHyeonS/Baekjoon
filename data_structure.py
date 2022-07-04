12cecc731cedbeb1d47083c1d933d1d383febc15
jungHyeonS <fbtls25@naver.com>
2022-07-04 18:26:02 +0900

    dfs 아이스크림 얼리기

--- data_structure.py
+++ data_structure.py
@@ -1,25 +1,35 @@
+
+# 노드 구현 클래스
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
+
+# 링크드 리스트 클래스
 class LinkedList:
+
+    # 링크드 리스트 클래스 호출시 head 값 세팅
     def __init__(self, data):
         self.head = Node(data)
 
 
-    def add(self,data):
+    # 현재 헤드값 부터 탐색을 진행해서 다음 노드가 없을경우 노드 추가
+    def addppend(self,data):
         cur = self.head
         while cur.next is not None:
             cur = cur.next
         cur.next = Node(data)
 
+
+    # 현재 헤드 기준으로 모든 노드값 출력
     def print_all(self):
         cur = self.head
         while cur is not None:
             print(cur.data)
             cur = cur.next
 
+    # 인덱스에 맞는 노드 구하기
     def get_node(self,index):
         cnt = 0
         node = self.head
@@ -28,22 +38,39 @@ class LinkedList:
             node = node.next
         return node
 
+    # 노드 삽입(인덱스를 기준으로)
     def add_node(self,index,value):
+        # 전달 받은 데이터로 새로운 노드 생성
         new_node = Node(value)
+
+        # 만약 index가 0이라면 새로 만든 현재 헤더가 되고 현재 헤드는 새로만든 노드가 된다 
         if index == 0:
             new_node.next = self.head
             self.head = new_node
             return
+        # 0이 아닐경우 삽입할려는 인덱스에 이전 노드를 가져오고
         node = self.get_node(index-1)
+
+        # 삽입할려는 인덱스 이전 노드에 next 노드 값을 저장
         next_node = node.next
+
+        # 삽입할려는 인덱스에 이전 노드에 next는 새로 삽입한 노드가 되고
         node.next = new_node
+
+        #새로 삽입한 노드에 next는 저장해두었던 인덱스 이전에 노드에 next
         new_node.next = next_node
 
+
+    # 노드 삭제(인덱스를 기준으로)
     def delete_node(self,index):
+        # 맨앞을 삭제한다면 현재 헤드의 다음 노드가 헤드가 된다
         if index == 0:
             self.head = self.head.next
             return
+        #맨 앞이 아닌 중간에 노드 라면 인덱스 기준으로 이전 노드를 구하고
         node = self.get_node(index-1)
+
+        #인적 노드에 next 값을 이전 노드에서 두칸 앞에 있는 노드
         node.next = node.next.next
 
 
@@ -51,10 +78,10 @@ class LinkedList:
 
 
 link = LinkedList(1)
-link.add(2)
-link.add(5)
-link.add(6)
-link.add(7)
+link.addppend(2)
+link.addppend(5)
+link.addppend(6)
+link.addppend(7)
 link.delete_node(3)
 link.print_all()
 # print()
--- dfs_bfs.py
+++ dfs_bfs.py
@@ -32,7 +32,7 @@
 
 
 # visited = [False] * 9
-# bfs(grpah,1,visited)
+# dfs(grpah,1,visited)
 
 
 # n,m = map(int,input().split())
@@ -67,16 +67,71 @@
 
 
 
-n,m = map(int,input().split())
+# n,m = map(int,input().split())
 
-graph = []
-for i in range(n):
-    graph.append(list(map(int,input())))
+# graph = []
+# for i in range(n):
+#     graph.append(list(map(int,input())))
 
     
-# print(graph)
+# # print(graph)
 
 
+# for i in range(n):
+#     for j in range(m):
+#         print(graph[i][j])
+
+
+
+
+
+
+# 음료수 얼려먹기
+# 입력간이 0인것만 방문
+# 0인거를 방문했으면 1로 변경
+
+
+n,m = map(int,input().split())
+
+# 2차원 리스트로 맵정보 입력
+grpah = []
+for i in range(n):
+    grpah.append(list(map(int,input())))
+
+
+# 특정 노드를 방문후에 그 노드와 연결되어있는(0) 을 모두 방문
+def dfs(x,y):
+
+    # 범위를 벗어난경우 종료
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+
+    #현재 노드를 방문하지 않았을경우
+    if grpah[x][y] == 0:
+        # 해당 노드를 방문 처리
+        grpah[x][y] = 1
+
+        # 해당 노드를 기준으로 상,하,좌,우 노드를 방문
+        dfs(x - 1,y)
+        dfs(x,y-1)
+        dfs(x + 1,y)
+        dfs(x,y+1)
+        
+        # 여기가 중요 0,0 
+        # 00110
+        # 00011
+        # 11111
+        # 00000
+        # 해당 맵 기준으로 0,0 부터 방문하여 연결되어있는 모든 0의 노드를을 연결처리 시키면
+        # 아래 반복문에서 묶음처리가 되어 3개가 생김
+        return True
+    return False
+
+
+result = 0
 for i in range(n):
     for j in range(m):
-        print(graph[i][j])
+        if dfs(i,j) == True:
+            result += 1
+
+print(result)
